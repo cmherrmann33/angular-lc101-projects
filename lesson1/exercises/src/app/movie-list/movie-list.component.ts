06f3985ae2100a6a858fb198bf8f7f6347710334
Caitlin Herrmann <cmherrmann33@gmail.com>
2020-07-29 19:16:50 -0500

    Committed last class work

--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['Pride and Prejudice', 'Binging The Office', 'Shes the Man', 'Popstar: Never Stop Never Stopping','Contagion'];
 
    constructor() { }
 
